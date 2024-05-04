72946d0a156496227b8a9404183869dcbcb0bf3d
janhavi22 <janhavijchitade@gmail.com>
2024-05-04 22:20:01 +0530

    new

--- src/componants/Home.jsx
+++ src/componants/Home.jsx
@@ -15,7 +15,6 @@ const Home = () => {
       <script src="https://cdn.tailwindcss.com"></script>
     <script src="https://cdn.tailwindcss.com?plugins=forms,typography,aspect-ratio,line-clamp"></script>
 
-    
      <div className="navbar1">
      <img src={imglogo} className="" alt="Tailwind Play" />
       <a href="/">Cart</a>
