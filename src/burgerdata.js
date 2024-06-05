6d542237bab419b6443c85b8ddc8d5591553156b
janhavi22 <janhavijchitade@gmail.com>
2024-06-05 19:29:37 +0530

    new

--- src/burgerdata.js
+++ src/burgerdata.js
@@ -5,51 +5,51 @@
 // import img5 from '../src/images/b5.jpg';
 // import img6 from '../src/images/b6.jpg';
 const burger=[{
-	"name":"Veggie burger",
-	"varients":["small","medium","large"],
-	"prices":[{"small":70,"medium":100,"large":120}],
-	"category":"nonveg",
-	"image":'https://img.freepik.com/free-photo/grilled-cheeseburger-sesame-bun-with-fresh-toppings-generative-ai_188544-12325.jpg?size=626&ext=jpg&ga=GA1.1.828808497.1679389173&semt=sph',
-	"description":""
+	name:"Veggie burger",
+	varients:["small","medium","large"],
+	prices:[{"small":70,"medium":100,"large":120}],
+	category:"nonveg",
+	image:'https://img.freepik.com/free-photo/grilled-cheeseburger-sesame-bun-with-fresh-toppings-generative-ai_188544-12325.jpg?size=626&ext=jpg&ga=GA1.1.828808497.1679389173&semt=sph',
+	description:""
 },{
-	"name":"special burger",
-	"varients":["small","medium","large"],
-	"prices":[{"small":70,"medium":100,"large":120}],
-	"category":"nonveg",
-	"image":'https://img.freepik.com/free-photo/grilled-gourmet-burger-with-cheese-tomato-onion-french-fries-generated-by-artificial-intelligence_25030-63181.jpg',
-	"description":""
+	name:"special burger",
+	varients:["small","medium","large"],
+	prices:[{"small":70,"medium":100,"large":120}],
+	category:"nonveg",
+	image:'https://img.freepik.com/free-photo/grilled-gourmet-burger-with-cheese-tomato-onion-french-fries-generated-by-artificial-intelligence_25030-63181.jpg',
+	description:""
 
 },{
-	"name":"spicy burger",
-	"varients":["small","medium","large"],
-	"prices":[{"small":70,"medium":100,"large":120}],
-	"category":"nonveg",
-	"image":'https://img.freepik.com/free-photo/grilled-burger-french-fries-food-generative-ai_188544-8516.jpg?size=626&ext=jpg&ga=GA1.1.828808497.1679389173&semt=sph',
-	"description":""
+	name:"spicy burger",
+	varients:["small","medium","large"],
+	prices:[{"small":70,"medium":100,"large":120}],
+	category:"nonveg",
+	image:'https://img.freepik.com/free-photo/grilled-burger-french-fries-food-generative-ai_188544-8516.jpg?size=626&ext=jpg&ga=GA1.1.828808497.1679389173&semt=sph',
+	description:""
 
 },{
-	"name":"cheese burger",
-	"varients":["small","medium","large"],
-	"prices":[{"small":70,"medium":100,"large":120}],
-	"category":"nonveg",
-	"image":'https://img.freepik.com/premium-photo/beef-burger-with-melted-cheese-bacon-wooden_197589-1600.jpg?size=626&ext=jpg&ga=GA1.1.828808497.1679389173&semt=sph',
-	"description":""
+	name:"cheese burger",
+	varients:["small","medium","large"],
+	prices:[{"small":70,"medium":100,"large":120}],
+	category:"nonveg",
+	image:'https://img.freepik.com/premium-photo/beef-burger-with-melted-cheese-bacon-wooden_197589-1600.jpg?size=626&ext=jpg&ga=GA1.1.828808497.1679389173&semt=sph',
+	description:""
 
 },{
-	"name":"hot spice burger",
-	"varients":["small","medium","large"],
-	"prices":[{"small":70,"medium":100,"large":120}],
-	"category":"nonveg",
-	"image":'https://img.freepik.com/free-photo/grilled-beef-burger-with-cheese-fries-generated-by-ai_24640-82256.jpg?size=626&ext=jpg&ga=GA1.1.828808497.1679389173&semt=sph',
-	"description":""
+	name:"hot spice burger",
+	varients:["small","medium","large"],
+	prices:[{"small":70,"medium":100,"large":120}],
+	category:"nonveg",
+	image:'https://img.freepik.com/free-photo/grilled-beef-burger-with-cheese-fries-generated-by-ai_24640-82256.jpg?size=626&ext=jpg&ga=GA1.1.828808497.1679389173&semt=sph',
+	description:""
 
 },{
-	"name":"jumbo burger",
-	"varients":["small","medium","large"],
-	"prices":[{"small":70,"medium":100,"large":120}],
-	"category":"nonveg",
-	"image":'https://img.freepik.com/premium-photo/fresh-tasty-homemade-burger-wooden-table_147620-1310.jpg?size=626&ext=jpg&ga=GA1.1.828808497.1679389173&semt=sph',
-	"description":""
+	name:"jumbo burger",
+	varients:["small","medium","large"],
+	prices:[{"small":70,"medium":100,"large":120}],
+	category:"nonveg",
+	image:'https://img.freepik.com/premium-photo/fresh-tasty-homemade-burger-wooden-table_147620-1310.jpg?size=626&ext=jpg&ga=GA1.1.828808497.1679389173&semt=sph',
+	description:""
 
 },
 ];
--- src/componants/Burger.js
+++ src/componants/Burger.js
@@ -1,19 +1,26 @@
 import React ,{useState} from 'react'
-
+// import {Modal} from 'react-bootstrap'
 
 function Burger({burger}) {
     const [ quantity , setquantity]= useState(1)
     const [ varient , setvarient]= useState('small')
+
+    // const [show, setShow] = useState(false);  
+    // const handleClose = () => setShow(false);
+    // const handleShow = () => setShow(true);
   return (
     
                
-    <div className='yoo'>
+    <div className='main-container'>
       <div className="container">
      
      
+     <center>
      <div className="burger-card">
-         <center><img src={burger.image} alt="Burger"/></center>
+        {/* <div onClick={handleShow}> */}
+        <center><img src={burger.image} alt="Burger"/></center>
          <h1>{burger.name}</h1>
+        {/* </div> */}
          <div className='sidebox' >
            <div>
                <p>Varients</p>
@@ -35,15 +42,29 @@ function Burger({burger}) {
 
          </div>
          <div className='sidebox'>
-           <div><h1>Price: {burger.prices[0][varient]*quantity}</h1></div>
+           <div><h1>Price: {burger.prices[0][varient]*quantity} Rs/-</h1></div>
            <div><button className='cartbtn'>Add to Cart</button></div>
          </div>
          
      </div>
+     </center>
        
        
     
    </div>
+   {/* <Modal show={show}>
+        <Modal.Header closeButton>
+          <Modal.Title>{burger.name}</Modal.Title>
+        </Modal.Header>
+
+        <Modal.Body>
+         <center><img src={burger.image} alt="Burger"/></center>
+        </Modal.Body>
+
+        <Modal.Footer>
+          <button className='modalbtn' onClick={handleClose}>Close</button>
+        </Modal.Footer>
+      </Modal> */}
 
 
     </div>
@@ -52,4 +73,4 @@ function Burger({burger}) {
   )
 }
 
-export default Burger
+export default Burger;
