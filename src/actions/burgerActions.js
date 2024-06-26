dff846cc07b57d29dfe5d09f9ba835b89e8e6ec3
janhavi22 <janhavijchitade@gmail.com>
2024-06-26 21:59:49 +0530

    challenges

--- src/actions/burgerActions.js
+++ src/actions/burgerActions.js
@@ -1,13 +1,12 @@
 import axios from "axios";
-export const getAllBurgers=()=>async dispatch=>{
+export const getAllBurgers = () => async (dispatch) => {
+  dispatch({ type: "GET_BURGERS_REQUEST" });
 
-    dispatch({type:'GET_BURGERS_REQUEST'})
-
-    try{
-        const response =await axios.get('/api/burgers/getallburgers')
-        console.log(response)
-        dispatch({type:'GET_BURGERS_SUCCESS',payload :response.data})
-    }catch(error){
-        dispatch({type:'GET_BURGERS_FAILED',payload:error})
-    }
-}
+  try {
+    const response = await axios.get("/api/burgers/getallburgers");
+    console.log(response);
+    dispatch({ type: "GET_BURGERS_SUCCESS", payload: response.data });
+  } catch (error) {
+    dispatch({ type: "GET_BURGERS_FAILED", payload: error });
+  }
+};
--- src/reducers/burgerReducer.js
+++ src/reducers/burgerReducer.js
@@ -1,15 +1,18 @@
-export const getAllBurgersReducer=(state={} ,action)=>{
-    switch(action.type){
-        case 'GET_BURGERS_REQUEST':return{
-            ...state
-        }
-        case 'GET_BURGERS_SUCCESS': return{
-            burger : action.payload
-        }
-        case 'GET_BURGERS_FAILED': return{
-            error : action.payload
-        }
-        default : return state
-
-    }
-}
+export const getAllBurgersReducer = (state = {}, action) => {
+  switch (action.type) {
+    case "GET_BURGERS_REQUEST":
+      return {
+        ...state,
+      };
+    case "GET_BURGERS_SUCCESS":
+      return {
+        burger: action.payload,
+      };
+    case "GET_BURGERS_FAILED":
+      return {
+        error: action.payload,
+      };
+    default:
+      return state;
+  }
+};
